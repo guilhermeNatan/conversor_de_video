bd526361dff2b803adfb12d519a51729cb117961
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2018-09-01 17:31:56 -0300

    First commit

---
+++ src/main/java/com/conversorvideo/conversorvideo/ConversorvideoApplication.java
@@ -0,0 +1,12 @@
+package com.conversorvideo.conversorvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorvideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorvideoApplication.class, args);
+	}
+}
