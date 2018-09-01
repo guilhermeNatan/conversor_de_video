2e20c39447d4d17fe41b3fa8a336a5affad4131b
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2018-09-01 19:16:29 -0300

    Configuração do projeto e dependecias.

--- src/main/java/com/conversorvideo/ConversorvideoApplication.java
+++ src/main/java/com/conversorvideo/ConversorvideoApplication.java
@@ -1,4 +1,4 @@
-package com.conversorvideo.conversorvideo;
+package com.conversorvideo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/conversorvideo/ConversorvideoApplicationTests.java
+++ src/test/java/com/conversorvideo/ConversorvideoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.conversorvideo.conversorvideo;
+package com.conversorvideo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
