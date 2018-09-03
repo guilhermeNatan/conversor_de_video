f22ffb4e90b5eae1ddc9898261389f7e5bae76d4
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2018-09-03 18:34:27 -0300

    Configuração do spring. (CORS)

---
+++ src/main/java/com/conversorvideo/configuration/SpringConfiguration.java
@@ -0,0 +1,28 @@
+package com.conversorvideo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class SpringConfiguration  {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("POST","GET","PUT", "DELETE");
+            }
+        };
+    }
+}
